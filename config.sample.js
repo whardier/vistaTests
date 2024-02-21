d3c200c8cfed108fbb90c18283e1dd6cd0f0eee7
Shane Elliott <shane.elliott@va.gov>
2024-02-21 10:18:46 -0800

    updated sample

--- config.sample.js
+++ config.sample.js
@@ -1,10 +1,22 @@
 module.exports = {
   key:'',
+  keyStaging:'',
   vistaApi:{
     url:'',
     tokenUrl:''
   },
+  vistaApiStaging:{
+    url:'',
+    tokenUrl:''
+  },
+  stagingKeyX:'',
+  stagingVistaApiX:{
+    url:'',
+    tokenUrl:''
+  },
+  KeyX:'',
+VistaApiX:{
+    url:'',
+    tokenUrl:''
+  },
 };
-
-
-
